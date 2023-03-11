322e093dae2cd063397d656182b37279e8172da8
Chidike Henry <dk1online25@gmail.com>
2023-03-11 16:30:40 +0100

    installed tailwind and added new feat

---
+++ pages/api/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.modules.css";
+
+const Home: NextPage = () => {
+    return (
+        <div>
+            <Head>
+                <title>Chidike Henry's - Portfolio</title>
+            </Head>
+
+            <h1>Lets build an Awesome Portfolio</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,94 @@
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { motion } from 'framer-motion'
+import About from "../components/About";
+import ContactMe from "../components/ContactMe";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
+import WorkExperience from "../components/WorkExperience";
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchSocials } from "../utils/fetchSocials";
+
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
+
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
+  return (
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <Head>
+        <title>Chidike Henry - Portfolio</title>
+      </Head>
+
+      <Header socials={socials} />
+
+      <section id="hero" className="snap-start">
+        <Hero pageInfo={pageInfo} />
+      </section>
+
+      <section id="about" className="snap-center">
+        <About pageInfo={pageInfo} />
+      </section>
+
+      <section id="experience" className="snap-center">
+        <WorkExperience experiences={experiences} />
+      </section>
+
+      <section id="skills" className="snap-start">
+        <Skills skills={skills} />
+      </section>
+
+      <section id="projects" className="snap-start">
+        <Projects projects={projects} />
+      </section>
+
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <motion.img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://avatars.githubusercontent.com/u/35535871?s=400&u=f37147801d9422aa533f66dcb655b62b349fcd05&v=4"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperiences();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Social[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  };
+};
