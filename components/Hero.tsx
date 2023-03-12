390f6b8f7b99287185c54ec5a22f6bf9d295e5a7
Chidike Henry <dk1online25@gmail.com>
2023-03-12 23:30:53 +0100

    add new feat

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
 
-type Props = {}
+type Props = {};
 
-export default function Hero({ }: Props) {
-    const [text, count] = useTypewriter({
-        words: ["Hi, The Name's Chidike Henry",'An experienced perfomance driven Engineer .tsx', '<And Loves to Code />'],
-    })
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's Chidike Henry",
+      "An experienced performance driven Engineer.tsx",
+      "<And Loves to Code />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
   return (
-    <div>Hero</div>
-  )
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+       <motion.img className="relative rounded-full h-32 w-32 mx-auto object-cover" src={urlFor(pageInfo?.heroImage).url()} alt="Logo" />
+          <div className="z-20">
+              <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">{pageInfo?.role}</h2>
+
+              <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+      </div>
+          
+    </div>
+  );
 }
--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -6,7 +6,7 @@ import Hero from "../../components/Hero";
 
 const Home: NextPage = () => {
     return (
-        <div>
+        <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
             <Head>
                 <title>Chidike Henry's - Portfolio</title>
             </Head>
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
             <Header />
 
             {/* Hero */}
-            <section id="hero">
+            <section id="hero" className="snap-center">
                 <Hero />
             </section>
             {/* About */}
